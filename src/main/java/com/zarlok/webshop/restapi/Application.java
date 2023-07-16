241452fd262119073e0c86c73e3a9bd0f6ff1e7f
Bartosz Żarłok <43600627+D3XT3RR@users.noreply.github.com>
2023-07-16 15:14:08 +0200

    Changing available Actuator end-points.

--- src/main/java/com/zarlok/webshop/restapi/Application.java
+++ src/main/java/com/zarlok/webshop/restapi/Application.java
@@ -2,12 +2,8 @@ package com.zarlok.webshop.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
