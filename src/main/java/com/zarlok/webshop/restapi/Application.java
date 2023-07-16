67e63f1c81a1242ab828a2f1926ad044b08fa316
Bartosz Żarłok <43600627+D3XT3RR@users.noreply.github.com>
2023-07-16 15:13:42 +0200

    Changing available Actuator end-points.

--- src/main/java/com/zarlok/webshop/restapi/Application.java
+++ src/main/java/com/zarlok/webshop/restapi/Application.java
@@ -15,7 +15,7 @@ public class Application {
 	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
 
 	@Bean
-	public PasswordEncoder passwordEncoder(){
+	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
 
